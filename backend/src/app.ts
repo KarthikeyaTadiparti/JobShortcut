7822b8db2e1e13ad4a1b3ed0a6a4bbb78fdb7d52
KarthikeyaTadiparti <kartikeyatadiparti@gmail.com>
2026-06-21 14:20:03 +0530

    Add cors middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import "dotenv/config";
 import authRouter from "./routes/auth-routes.js";
 import adminRouter from "./routes/admin-routes.js";
@@ -9,6 +10,12 @@ import morganMiddleware from "./middlewares/morgan.js";
 const app = express();
 
 // Standard middlewares
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(morganMiddleware);
 app.use(express.json());
 app.use(cookieParser());
